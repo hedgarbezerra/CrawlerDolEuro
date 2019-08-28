ad0ef6f5c856fcd39115c9a3418889d48912d98c
hedgarbezerra <hedgarbezerra35@gmail.com>
2019-08-28 13:12:03 -0300

    Initial commit

---
+++ CrawDol/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace CrawDol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDolar = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtDolar = new System.Windows.Forms.TextBox();
+            this.txtEuro = new System.Windows.Forms.TextBox();
+            this.lblEuro = new System.Windows.Forms.Label();
+            this.cbEscolha = new System.Windows.Forms.ComboBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDolar
+            // 
+            this.lblDolar.AutoSize = true;
+            this.lblDolar.Location = new System.Drawing.Point(50, 74);
+            this.lblDolar.Name = "lblDolar";
+            this.lblDolar.Size = new System.Drawing.Size(72, 13);
+            this.lblDolar.TabIndex = 0;
+            this.lblDolar.Text = "Valor do dolar";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(191, 258);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(105, 43);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Converter";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtDolar
+            // 
+            this.txtDolar.Enabled = false;
+            this.txtDolar.Location = new System.Drawing.Point(12, 90);
+            this.txtDolar.Name = "txtDolar";
+            this.txtDolar.ReadOnly = true;
+            this.txtDolar.Size = new System.Drawing.Size(145, 20);
+            this.txtDolar.TabIndex = 2;
+            // 
+            // txtEuro
+            // 
+            this.txtEuro.Enabled = false;
+            this.txtEuro.Location = new System.Drawing.Point(12, 149);
+            this.txtEuro.Name = "txtEuro";
+            this.txtEuro.ReadOnly = true;
+            this.txtEuro.Size = new System.Drawing.Size(145, 20);
+            this.txtEuro.TabIndex = 4;
+            // 
+            // lblEuro
+            // 
+            this.lblEuro.AutoSize = true;
+            this.lblEuro.Location = new System.Drawing.Point(50, 133);
+            this.lblEuro.Name = "lblEuro";
+            this.lblEuro.Size = new System.Drawing.Size(70, 13);
+            this.lblEuro.TabIndex = 3;
+            this.lblEuro.Text = "Valor do euro";
+            this.lblEuro.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // cbEscolha
+            // 
+            this.cbEscolha.Location = new System.Drawing.Point(279, 74);
+            this.cbEscolha.Name = "cbEscolha";
+            this.cbEscolha.Size = new System.Drawing.Size(175, 21);
+            this.cbEscolha.TabIndex = 5;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(279, 133);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(187, 20);
+            this.txtInput.TabIndex = 6;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Enabled = false;
+            this.txtOutput.Location = new System.Drawing.Point(279, 185);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(187, 20);
+            this.txtOutput.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(186, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 28);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Conversão";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Opção de conversão";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(348, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Saída";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(349, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Entrada";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 339);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.cbEscolha);
+            this.Controls.Add(this.txtEuro);
+            this.Controls.Add(this.lblEuro);
+            this.Controls.Add(this.txtDolar);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblDolar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDolar;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtDolar;
+        private System.Windows.Forms.TextBox txtEuro;
+        private System.Windows.Forms.Label lblEuro;
+        private System.Windows.Forms.ComboBox cbEscolha;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CrawDol/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrawDol
+{
+    public partial class Form1 : Form
+    {
+
+        private string Dolar
+        {
+            get { return getDolar();}
+        }
+
+        private string Euro
+        {
+            get { return getEuro(); }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getDolar()
+        {
+            WebClient wCliente = new WebClient();
+            wCliente.Headers.Add("User-Agent:Other");
+            wCliente.Encoding = System.Text.Encoding.UTF8;
+
+            var data = wCliente.DownloadString("https://economia.uol.com.br/");
+
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(data);
+
+            var ret = document.DocumentNode.SelectNodes("//div[@class='hidden-xs']/h3[@class='tituloGrafico']/a[@class='subtituloGrafico subtituloGraficoValor']");
+            var retorno = ret[0].InnerText;
+
+            return retorno;
+        }
+
+        private string getEuro()
+        {
+            WebClient wCliente = new WebClient();
+            wCliente.Headers.Add("User-Agent:Other");
+            wCliente.Encoding = System.Text.Encoding.UTF8;
+
+            var data = wCliente.DownloadString("https://economia.uol.com.br/");
+
+            HtmlAgilityPack.HtmlDocument document = new HtmlAgilityPack.HtmlDocument();
+            document.LoadHtml(data);
+
+            var ret = document.DocumentNode.SelectNodes("//div[@class='hidden-xs']/h3[@class='tituloGrafico']/a[@class='subtituloGrafico subtituloGraficoValor']");
+            var retorno = ret[1].InnerText;
+
+            return retorno;
+        }
+
+        private decimal Conversao()
+        {
+
+            var input = Convert.ToDecimal(txtInput.Text.Replace("R$", ""), new CultureInfo("pt-BR"));
+
+            if (cbEscolha.Text == "Euro")
+            {
+                var moeda = Convert.ToDecimal(this.Euro.Replace("R$", ""), new CultureInfo("pt-BR"));
+
+                return moeda* input;
+            }
+
+            else
+            {
+                var moeda = Convert.ToDecimal(this.Dolar.Replace("R$", ""), new CultureInfo("pt-BR"));
+
+                return moeda * input;
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string objFinal = Conversao().ToString();
+            txtOutput.Text = objFinal;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbEscolha.Items.Add("Dolar");
+            cbEscolha.Items.Add("Euro");
+            txtDolar.Text = this.Dolar;
+            txtEuro.Text = this.Euro;
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
